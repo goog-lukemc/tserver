c8903dce571363f1f525e2f1e0209aa44d3d03d2
Luke McCoy <lukemc@google.com>
2020-05-08 13:04:20 -0400

    Updated pointer issue

--- tserver.go
+++ tserver.go
@@ -91,7 +91,7 @@ func NewServer(cfg *ServerConfig) *ServerControl {
 }
 
 // GetRequestBody decode the body of the server to the target type
-func GetRequestBody(w http.ResponseWriter, r *http.Request, target *interface{}) error {
+func GetRequestBody(w http.ResponseWriter, r *http.Request, target interface{}) error {
 	// Check if the the http method is a post request.
 	if r.Method != "POST" {
 		http.Error(w, "errBadHTTPMethod", http.StatusMethodNotAllowed)
